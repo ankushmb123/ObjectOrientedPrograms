775b05bf853c34d69f4e01c8c183caf3c5e51d31
Ankush Bahekar <ankushmb9@gmail.com>
2022-03-12 15:09:18 +0530

    [Ankush] Add: Output JSON string

---
+++ InventoryManagementData/InventoryManagementData/FetchDataForRice.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace InventoryManagementData
+{
+    public class FetchDataForRice
+    {
+        public Rice Read(string Path)
+        {
+            using (StreamReader file = new StreamReader(Path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Rice>(json);
+
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ InventoryManagementData/InventoryManagementData/Rice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementData
+{
+    public class Rice
+    {
+        public List<TypesOfRice> typesOfRice;
+        public List<TypesOfGrains> typesOfGrains;
+        public List<TypesOfWheats> typesOfWheats;
+    }
+
+
+    public class TypesOfRice
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Price { get; set; }
+    }
+
+    public class TypesOfGrains
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Price { get; set; }
+    }
+
+    public class TypesOfWheats
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Price { get; set; }
+    }
+}
